1dd79a0848db240619c1efd97ca8efafeed1a5b1
t3sserakt <t3ss@posteo.de>
2021-11-22 19:03:38 +0100

    - fix wrong kernel parameter check. Fix double free of GNUNET_HELPER_SendHandle

--- src/transport/test_transport_udp_backchannel.sh
+++ src/transport/test_transport_udp_backchannel.sh
@@ -2,7 +2,7 @@
 if ! [ -d "/run/netns" ]; then
     echo You have to create the directory /run/netns.
 fi
-if  [ "$(cat /proc/sys/kernel/core_uses_pid)" == 1 ]; then
+if  [ "$(cat /proc/sys/kernel/unprivileged_userns_clone)" == 1 ]; then
 #exec unshare -r -nmU bash -c "mount -t tmpfs --make-rshared tmpfs /run/netns; valgrind --leak-check=full --track-origins=yes --trace-children=yes --trace-children-skip=/usr/bin/awk,/usr/bin/cut,/usr/bin/seq,/sbin/ip ./test_transport_start_with_config test_transport_udp_backchannel_topo.conf"
 exec unshare -r -nmU bash -c "mount -t tmpfs --make-rshared tmpfs /run/netns; ./test_transport_start_with_config test_transport_udp_backchannel_topo.conf"
 # exec unshare -r -nmU bash -c "mount -t tmpfs --make-rshared tmpfs /run/netns; valgrind --leak-check=full --track-origins=yes ./test_transport_start_with_config test_transport_udp_backchannel_topo.conf"
