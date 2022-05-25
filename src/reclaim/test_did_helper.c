a3af7211c33d6f54d3b72c37c1abe28dad89667f
Tristan Schwieren <tristan.schwieren@tum.de>
2022-05-25 14:07:03 +0200

    - init did test

---
+++ src/reclaim/test_did_helper.c
@@ -0,0 +1,35 @@
+/*
+   This file is part of GNUnet
+   Copyright (C) 2010-2015 GNUnet e.V.
+
+   GNUnet is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+
+   GNUnet is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   SPDX-License-Identifier: AGPL3.0-or-later
+ */
+
+/**
+ * @file reclaim/test_did_helper.c
+ * @brief Unit tests for the helper library for DID related functions
+ * @author Tristan Schwieren
+ */
+
+#include "platform.h"
+#include "gnunet_common.h"
+
+int
+main()
+{
+    GNUNET_assert(0 == 1);
+    return 0;
+}
