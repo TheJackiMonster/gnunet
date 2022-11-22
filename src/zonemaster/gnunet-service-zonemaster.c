af4be43d9e4f6140287860a993d655d5bf2c37f9
Martin Schanzenbach <schanzen@gnunet.org>
2022-11-22 15:17:41 +0900

    -fix

--- src/zonemaster/gnunet-service-zonemaster.c
+++ src/zonemaster/gnunet-service-zonemaster.c
@@ -781,7 +781,8 @@ dht_put_continuation (void *cls)
               "PUT complete; Pending jobs: %u\n", job_queue_length - 1);
   /* When we just fall under the limit, trigger monitor/iterator again
    * if halted. We can only safely trigger one, prefer iterator. */
-  check_zone_namestore_next ();
+  if (NULL != zone_publish_task)
+    check_zone_namestore_next ();
   if (job_queue_length <= JOB_QUEUE_LIMIT)
   {
     if (GNUNET_YES == monitor_halted)
