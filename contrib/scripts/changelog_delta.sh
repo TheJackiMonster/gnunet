715ca9df95320ccb5edc8982b264cb41774b7ccc
Martin Schanzenbach <schanzen@gnunet.org>
2022-12-08 15:39:35 +0900

    - add indent to HTML output

--- contrib/scripts/changelog_delta.sh
+++ contrib/scripts/changelog_delta.sh
@@ -3,7 +3,7 @@ if [ $2 == "plain" ]; then
   FORMAT="  - %s%n%b"
   echo "Changes since $1:"
 elif [ $2 == "html" ]; then
-  FORMAT="<li>%s<br>%b</li>"
+  FORMAT="  <li>%s<br>%b</li>"
   echo "<ul>"
 elif [ $2 == "changelog" ]; then
   FORMAT="%aD (%h)%n%s%n%b%nby: %cN%n"
