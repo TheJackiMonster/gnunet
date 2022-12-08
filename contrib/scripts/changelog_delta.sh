2d257589da20487334447fe33616bdd7a27a79ef
Martin Schanzenbach <schanzen@gnunet.org>
2022-12-08 15:37:43 +0900

    BUILD: Add functionality to generate changes in plain text and HTML

--- contrib/scripts/changelog_delta.sh
+++ contrib/scripts/changelog_delta.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-LASTHASH=$(head -n1 ChangeLog | cut -d " " -f 7 | tr -d \( | tr -d \))
-git log --grep="^[a-zA-Z]*: " --no-merges --no-color --format="%aD (%h)%n%s%n%b%nby: %cN%n" $LASTHASH..HEAD
+if [ $2 == "plain" ]; then
+  FORMAT="  - %s%n%b"
+  echo "Changes since $1:"
+elif [ $2 == "html" ]; then
+  FORMAT="<li>%s<br>%b</li>"
+  echo "<ul>"
+elif [ $2 == "changelog" ]; then
+  FORMAT="%aD (%h)%n%s%n%b%nby: %cN%n"
+fi
+
+git --no-pager log --grep="^[a-zA-Z]*: " --no-merges --no-color --format="$FORMAT" $1..HEAD
+
+if [ $2 == "html" ]; then
+  echo "</ul>"
+fi
+
+
