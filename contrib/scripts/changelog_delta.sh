884eb205f14fb1c021fb80a7207ce66373f453fc
Martin Schanzenbach <schanzen@gnunet.org>
2023-01-18 13:51:46 +0900

    build: change changelog scripts to NEWS.

--- contrib/scripts/changelog_delta.sh
+++ contrib/scripts/changelog_delta.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-if [ $2 == "plain" ]; then
-  FORMAT="  - %s%n%b"
-  echo "Changes since $1:"
-elif [ $2 == "html" ]; then
-  FORMAT="  <li>%s<br>%b</li>"
-  echo "<ul>"
-elif [ $2 == "changelog" ]; then
-  FORMAT="%aD (%h)%n%s%n%b%nby: %cN%n"
-fi
 
-git --no-pager log --grep="^[a-zA-Z]*: " --no-merges --no-color --format="$FORMAT" $1..HEAD
+OLDVERSION=$1
 
-if [ $2 == "html" ]; then
-  echo "</ul>"
-fi
-
-
+git --no-pager log --grep="^NEWS: " -i --no-merges --no-color --format="%s%n%b" $1..HEAD | grep -i "^NEWS:\s[a-zA-Z][a-zA-Z]*" | sed 's/NEWS:/  -/i'
+echo ""
--- contrib/scripts/update_changelog.sh
+++ contrib/scripts/update_changelog.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+NEW_VERSION=$1
+if [ -z $NEW_VERSION ]; then
+    NEW_VERSION="Unreleased"
+fi
 DELTA_SH="contrib/scripts/changelog_delta.sh"
-LASTHASH=$(head -n1 ChangeLog | cut -d " " -f 7 | tr -d \( | tr -d \))
+LASTHASH=$(head -n1 NEWS | cut -d " " -f 2 | tr -d \( | tr -d \) | tr -d :)
 
-$DELTA_SH $LASTHASH changelog > ChangeLog.delta || exit 1
-cp ChangeLog ChangeLog.bak || exit 1
-cat ChangeLog.delta > ChangeLog || exit 1
-cat ChangeLog.bak >> ChangeLog || exit 1
-rm ChangeLog.bak ChangeLog.delta
+echo "$NEW_VERSION ($(git rev-parse --short HEAD)):" > NEWS.delta || exit 1
+$DELTA_SH $LASTHASH >> NEWS.delta || exit 1
+cp NEWS NEWS.bak || exit 1
+cat NEWS.delta > NEWS || exit 1
+cat NEWS.bak >> NEWS || exit 1
+rm NEWS.bak NEWS.delta
 
