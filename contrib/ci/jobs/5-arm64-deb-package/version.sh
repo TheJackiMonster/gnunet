9dd915b5a0b59dfa2589ffc7f8e4ce3b60bdeafb
Devan Carpenter <git@dvn.me>
2023-11-27 22:36:51 -0500

    ci: update version script to work on detached head
    git version 2.4.4 changed the way rev-parsing worked in a detached head
    state

--- contrib/ci/jobs/5-arm64-deb-package/version.sh
+++ contrib/ci/jobs/5-arm64-deb-package/version.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -ex
 
-git fetch origin $(git rev-parse --abbrev-ref HEAD) --depth=1000 --tags
-RECENT_VERSION_TAG=$(git describe --tags --match 'v*.*.*' --always --abbrev=0 HEAD)
-
-commits="$(git rev-list ${RECENT_VERSION_TAG}..HEAD --count)"
-if [ "${commits}" = "0" ]; then
-    git describe --tag HEAD
+BRANCH=$(git name-rev --name-only HEAD)
+if [ -z "${BRANCH}" ]; then
+	exit 1
 else
-    echo $(echo ${RECENT_VERSION_TAG} | cut -d'v' -f2)-${commits}-$(git rev-parse --short=8 HEAD)
+        # "Unshallow" our checkout, but only our current branch, and exclude the submodules.
+	git fetch --no-recurse-submodules --tags --depth=1000 origin "${BRANCH}"
+	RECENT_VERSION_TAG=$(git describe --tags --match 'v*.*.*' --always --abbrev=0 HEAD)
+	commits="$(git rev-list ${RECENT_VERSION_TAG}..HEAD --count)"
+	if [ "${commits}" = "0" ]; then
+		git describe --tag HEAD
+	else
+		echo $(echo ${RECENT_VERSION_TAG} | sed -r 's/^v//')-${commits}-$(git rev-parse --short=8 HEAD)
+	fi
 fi
