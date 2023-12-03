db896ea3139decefb7ab642cbaf72c123b8b07ad
Sebastian Nadler <sebastian.nadler@tum.de>
2023-12-03 21:07:26 +0100

    improved readability

--- src/lib/util/dnsparser.c
+++ src/lib/util/dnsparser.c
@@ -1260,7 +1260,8 @@ GNUNET_DNSPARSER_builder_add_uri (char *dst,
   (*off) += sizeof(sd);
   strncpy(&dst[*off], uri->target, dst_len - sizeof(struct GNUNET_TUN_DnsUriRecord) - 1);
   (*off) += strlen(uri->target);
-  dst[*off++] = '\0'; 
+  dst[*off] = '\0';
+  *off++;
   return GNUNET_OK;
 }
 
