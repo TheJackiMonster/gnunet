3c6175c06b11987c08ce01d511a43e2ec1adee8f
Devan Carpenter <git@dvn.me>
2024-02-28 20:41:51 -0500

    ci: update run-local script

--- contrib/ci/ci.sh
+++ contrib/ci/ci.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
-set -evuo pipefail
+set -exvuo pipefail
 
-# Use podman if available, otherwise use docker.
-# Fails if neither is found in PATH
-OCI_RUNTIME=$(which podman || which docker)
+# Requires podman
+# Fails if not found in PATH
+OCI_RUNTIME=$(which podman)
 REPO_NAME=$(basename "${PWD}")
 JOB_NAME="${1}"
-JOB_CONTAINER=$((grep CONTAINER_NAME ci/jobs/${JOB_NAME}/config.ini | cut -d' ' -f 3) || echo "${REPO_NAME}")
+JOB_ARCH=$((grep CONTAINER_ARCH contrib/ci/jobs/${JOB_NAME}/config.ini | cut -d' ' -f 3) || echo "${2:-amd64}")
+JOB_CONTAINER=$((grep CONTAINER_NAME contrib/ci/jobs/${JOB_NAME}/config.ini | cut -d' ' -f 3) || echo "localhost/${REPO_NAME}:${JOB_ARCH}")
+CONTAINER_BUILD=$((grep CONTAINER_BUILD contrib/ci/jobs/${JOB_NAME}/config.ini | cut -d' ' -f 3) || echo "True")
 
-echo "${JOB_CONTAINER}"
+echo "Image name: ${JOB_CONTAINER}"
 
-if [ "${JOB_CONTAINER}" = "${REPO_NAME}" ] ; then
+if [ "${CONTAINER_BUILD}" = "True" ] ; then
 	"${OCI_RUNTIME}" build \
+		--arch "${JOB_ARCH}" \
 		-t "${JOB_CONTAINER}" \
-		-f ci/Containerfile .
+		-f contrib/ci/Containerfile .
 fi
 
 "${OCI_RUNTIME}" run \
 	--rm \
 	-ti \
+	--arch "${JOB_ARCH}" \
 	--env CI_COMMIT_REF="$(git rev-parse HEAD)" \
 	--volume "${PWD}":/workdir \
 	--workdir /workdir \
 	"${JOB_CONTAINER}" \
-	ci/jobs/"${JOB_NAME}"/job.sh
+	contrib/ci/jobs/"${JOB_NAME}"/job.sh
 
 top_dir=$(dirname "${BASH_SOURCE[0]}")
 
