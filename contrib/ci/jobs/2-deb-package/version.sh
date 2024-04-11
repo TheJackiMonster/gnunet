1508b682f52bf542bacd261757c36cbdfb04546a
Devan Carpenter <git@dvn.me>
2024-04-11 16:05:55 -0400

    ci: sanitize .deb version strings more thoroughly

--- contrib/ci/jobs/2-deb-package/version.sh
+++ contrib/ci/jobs/2-deb-package/version.sh
@@ -7,10 +7,10 @@ if [ -z "${BRANCH}" ]; then
 else
         # "Unshallow" our checkout, but only our current branch, and exclude the submodules.
 	git fetch --no-recurse-submodules --tags --depth=1000 origin "${BRANCH}"
-	RECENT_VERSION_TAG=$(git describe --tags --match 'v*.*.*' --always --abbrev=0 HEAD)
+	RECENT_VERSION_TAG=$(git describe --tags --match 'v*.*.*' --exclude '*-*dev*' --always --abbrev=0 HEAD || exit 1)
 	commits="$(git rev-list ${RECENT_VERSION_TAG}..HEAD --count)"
 	if [ "${commits}" = "0" ]; then
-		git describe --tag HEAD
+		git describe --tag HEAD | sed -r 's/^v//' || exit 1
 	else
 		echo $(echo ${RECENT_VERSION_TAG} | sed -r 's/^v//')-${commits}-$(git rev-parse --short=8 HEAD)
 	fi
