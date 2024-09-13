b07be15a40dd4d577c51a360ff185a5d89f7d4b5
Devan Carpenter <git@dvn.me>
2024-09-13 20:55:42 +0100

    ci: use env var for project name

--- contrib/ci/jobs/3-amd64-deploy-package/job.sh
+++ contrib/ci/jobs/3-amd64-deploy-package/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exuo pipefail
 
-ARTIFACT_PATH="/artifacts/exchange/${CI_COMMIT_REF}/*.deb"
+ARTIFACT_PATH="/artifacts/${CI_PROJECT_NAME}/${CI_COMMIT_REF}/*.deb"
 RELEASE_ARTIFACT_DIR="${RELEASE_ARTIFACT_DIR:-"$(dirname $0)/../../../../release-artifacts"}"
 
 if [[ -d "$RELEASE_ARTIFACT_DIR" ]]; then
