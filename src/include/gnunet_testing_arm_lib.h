38d891abbc367ce878888b5a72df85422b33136d
ch3 <ch3@mailbox.org>
2024-10-01 18:33:17 +0200

    testing-arm: expose configure with a trait

--- src/include/gnunet_testing_arm_lib.h
+++ src/include/gnunet_testing_arm_lib.h
@@ -37,7 +37,10 @@ GNUNET_TESTING_cmd_stop_peer (
 #define GNUNET_TESTING_ARM_SIMPLE_TRAITS(op, prefix) \
         op (prefix,                                  \
             arm_handle,                              \
-            struct GNUNET_ARM_Handle)
+            struct GNUNET_ARM_Handle)                \
+        op (prefix,                                  \
+            config,                                  \
+            const struct GNUNET_CONFIGURATION_Handle)
 
 
 GNUNET_TESTING_ARM_SIMPLE_TRAITS (GNUNET_TESTING_MAKE_DECL_SIMPLE_TRAIT,
