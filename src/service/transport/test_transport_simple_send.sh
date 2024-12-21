b3daaaca459f70b6a1ddce025dfa0b90fc565eb4
Martin Schanzenbach <schanzen@gnunet.org>
2024-12-21 17:12:38 +0100

    testing: try to replicate failing arm probnat test towards transport hello world test

--- src/service/transport/test_transport_simple_send.sh
+++ src/service/transport/test_transport_simple_send.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-exec netjail_test_master.sh ./test_transport_start_with_config test_transport_simple_send_topo.conf
-
-# exec netjail_test_master.sh gnunet-testing-netjail-launcher test_transport_simple_send_topo.conf
+exec ../../../scripts/netjail/netjail_test_master.sh gnunet-testing-netjail-launcher test_transport_simple_send_topo.conf
