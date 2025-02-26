7b394c81d698506e933b81f1d1563cf83d6cede4
Devan Carpenter <git@dvn.me>
2025-02-26 14:09:55 -0600

    ci: update build script to use meson

--- contrib/ci/common/build/build.sh
+++ contrib/ci/common/build/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -exuo pipefail
 
-make clean || true
-./bootstrap
-./configure CFLAGS="-ggdb -O0" \
-	    --enable-logging=verbose \
-	    --disable-doc
+./bootstrap meson
+meson setup -Dlogging=verbose \
+	    build
 
-make -j $(nproc)
-make install
+meson compile -C build
+meson install -C build
