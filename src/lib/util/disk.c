2b3c3659e76bd80abd4e932e28741d12b2c18099
Martin Schanzenbach <schanzen@gnunet.org>
2025-07-07 23:57:36 +0200

    util: simplify symlink resolution using realpath

--- src/lib/util/disk.c
+++ src/lib/util/disk.c
@@ -173,21 +173,11 @@ get_size_rec (void *cls, const char *fn)
   if ((S_ISLNK (buf.st_mode)) && (gfsd->include_sym_links == GNUNET_NO))
   {
     char linkdst[PATH_MAX];
-    char *fn_start;
-    ssize_t ret;
-    strcpy (linkdst, fn);
-    fn_start = strrchr (linkdst, '/');
-    if (!fn_start)
-      fn_start = linkdst;
-    else
-      fn_start++; // skip '/'
-    ret = readlink(fn, fn_start, PATH_MAX);
-    if (0 > ret)
+    if (NULL == realpath (fn, linkdst))
     {
-      LOG_STRERROR_FILE (GNUNET_ERROR_TYPE_DEBUG, "readlink", fn);
+      LOG_STRERROR_FILE (GNUNET_ERROR_TYPE_DEBUG, "realpath", fn);
       return GNUNET_SYSERR;
     }
-    fn_start[ret] = '\0';
     return get_size_rec(gfsd, linkdst);
   }
   if ((! S_ISLNK (buf.st_mode)) || (gfsd->include_sym_links == GNUNET_YES))
