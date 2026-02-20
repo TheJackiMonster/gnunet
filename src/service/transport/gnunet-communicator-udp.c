48def84e9e299c02d4b87149e9b93b0a59d7b54c
Jacki <jacki@thejackimonster.de>
2026-02-20 03:51:41 +0100

    -add year to copyright dates
    Signed-off-by: Jacki <jacki@thejackimonster.de>

--- src/service/transport/gnunet-communicator-udp.c
+++ src/service/transport/gnunet-communicator-udp.c
@@ -1,6 +1,6 @@
 /*
      This file is part of GNUnet
-     Copyright (C) 2010-2014, 2018, 2019 GNUnet e.V.
+     Copyright (C) 2010-2014, 2018, 2019, 2026 GNUnet e.V.
 
      GNUnet is free software: you can redistribute it and/or modify it
      under the terms of the GNU Affero General Public License as published
